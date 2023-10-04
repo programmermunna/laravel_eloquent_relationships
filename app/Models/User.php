f83a4d061dbe0ac4fdae160841d4999ca768c2de
Programmer Munna <programmermunna@gmail.com>
2023-10-04 21:29:23 +0600

    hasOne relation done

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function phone(){
+        return $this->hasOne(Phone::class,'user_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Phone;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $phone = User::find(3);
+    $phone = User::find(3)->phone;
+    // return $phone;
+    
+    $user = Phone::find(1);
+    $user = Phone::find(1)->user;
+    // return $user;
+
+    $phones = Phone::all();
+    $users = User::all();
+    // return $users;
+
+    return view('welcome',compact('users'));
 });
